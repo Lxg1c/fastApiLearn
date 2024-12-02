5e4bf0d095655103dfdc5d4129736c5d1c346002
Lxg1c <teran_2005@mail.ru>
2024-12-02 15:57:13 +0300

    Created model of Major

--- app/majors/models.py
+++ app/majors/models.py
@@ -1,18 +1,18 @@
 from sqlalchemy import text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database import Base, str_uniq, int_pk, str_null_true
-from app.students.models import Student
 
 
 # создаем модель таблицы факультетов (majors)
 class Major(Base):
-    id: Mapped[int_pk]
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     major_name: Mapped[str_uniq]
     major_description: Mapped[str_null_true]
     count_students: Mapped[int] = mapped_column(server_default=text('0'))
 
     # Определяем отношения: один факультет может иметь много студентов
-    students: Mapped[list[Student]] = relationship("Student", back_populates="major")
+    students: Mapped[list["Student"]] = relationship("Student", back_populates="major")
+    extend_existing = True
 
     def __str__(self):
         return f"{self.__class__.__name__}(id={self.id}, major_name={self.major_name!r})"
